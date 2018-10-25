5ca237ba973da55e6b12d329c968d1cec64aa31b
Martin de la Maza <medelamaza@miuandes.cl>
2018-10-25 15:50:51 -0300

    "hola"

---
+++ MVC/Comida.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC
+{
+    public class Comida
+    {
+        string Nombre;
+        int Carbohidratos, Proteinas, Grasas;        public void Calorias()
+        {
+
+        }
+    }
+}
